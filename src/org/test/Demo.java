3d13ad237011f7ae098d168c074af17091ea7855
udhay007 <udhaysmart8@gmail.com>
2022-01-04 19:33:37 +0530

    File Structure

---
+++ src/org/test/Demo.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public static void main(String[] args) {
+		List<Integer> l=new ArrayList<>();
+		
+		l.add(100);
+		l.add(200);
+		l.add(300);
+		l.add(400);
+		l.add(500);
+		
+		
+		
+		
+		
+	}
+	
+
+}
