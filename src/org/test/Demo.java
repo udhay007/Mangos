9fe53822a27998daa1fef764b2bfbdcde9a33540
udhay007 <96900814+udhay007@users.noreply.github.com>
2022-01-04 19:38:13 +0530

    Update Demo.java

--- src/org/test/Demo.java
+++ src/org/test/Demo.java
@@ -13,6 +13,23 @@ public class Demo {
 		l.add(400);
 		l.add(500);
 		
+		 System.out.println("has next");
+	   while (listIterator.hasNext()) {
+		Integer integer = (Integer) listIterator.next();
+		System.out.println(integer);
+		
+	}
+	   System.out.println("has previous");
+	   while (listIterator.hasPrevious()) {
+		Integer integer = (Integer) listIterator.previous();
+		System.out.println(integer);
+		
+		if (integer==200) {
+			listIterator.remove();
+		}
+	}
+	   System.out.println(l);
+}
 		
 		
 		
@@ -20,4 +37,4 @@ public class Demo {
 	}
 	
 
-}
+
